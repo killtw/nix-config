2f9d408da4b852aa2e426f40139795a2adcc2e49
Karl Li <killtw@gmail.com>
2024-10-28 16:20:24 +0800

    refactor: Move `homebrew` from `apps.nix` to `homebrew.nix`

--- modules/apps.nix
+++ modules/apps.nix
@@ -5,64 +5,9 @@
     wget
   ];
 
-  environment.variables.EDITOR = "vim";
-
-  homebrew = {
-    enable = true;
-
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "zap";
-    };
-
-    taps = [];
-
-    brews = [
-      "bitwarden-cli"
-      "warrensbox/tap/tfswitch"
-    ];
-
-    casks = [
-      "qlimagesize"
-      "qlmarkdown"
-      "qlstephen"
-      "quicklook-csv"
-      "quicklook-json"
-
-      "adguard"
-      "airbuddy"
-      "arc"
-      "ferdium"
-      "iina"
-      "itsycal"
-      "jordanbaird-ice"
-      "karabiner-elements"
-      "raycast"
-      "sonos"
-      "spotify"
-      "surge"
-      # "thor"
-
-      "dingtalk"
-      "google-cloud-sdk"
-      "lens"
-      "phpstorm"
-      "postman"
-      "tableplus"
-      "visual-studio-code"
-    ];
-
-    masApps = {
-      Bitwarden = 1352778147;
-      CodePiper = 1669959741;
-      Reeder = 1529448980;
-      Pages = 409201541;
-      "Spark Desktop" = 6445813049;
-      Keynote = 409183694;
-      LINE = 539883307;
-      "The Unarchiver" = 425424353;
-      PopClip = 445189367;
-      Numbers = 409203825;
-    };
+  environment.variables = {
+    EDITOR = "vim";
+    HOMEBREW_NO_ANALYTICS = "1";
+    HOMEBREW_NO_ENV_HINTS = "1";
   };
 }
--- modules/homebrew.nix
+++ modules/homebrew.nix
@@ -1,17 +1,75 @@
-{ inputs, ... }: {
+{ inputs, username, ... }: {
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      # autoUpdate = true;
+      cleanup = "zap";
+    };
+
+    global = {
+      autoUpdate = true;
+    };
+
+    taps = [];
+
+    brews = [
+      "bitwarden-cli"
+      "warrensbox/tap/tfswitch"
+    ];
+
+    casks = [
+      "qlimagesize"
+      "qlmarkdown"
+      "qlstephen"
+      "quicklook-csv"
+      "quicklook-json"
+
+      "adguard"
+      "airbuddy"
+      "arc"
+      "ferdium"
+      "iina"
+      "itsycal"
+      "jordanbaird-ice"
+      "karabiner-elements"
+      "raycast"
+      "sonos"
+      "spotify"
+      "surge"
+      # "thor"
+
+      "dingtalk"
+      "google-cloud-sdk"
+      "lens"
+      "phpstorm"
+      "postman"
+      "tableplus"
+      "visual-studio-code"
+    ];
+
+    masApps = {
+      Bitwarden = 1352778147;
+      CodePiper = 1669959741;
+      Reeder = 1529448980;
+      Pages = 409201541;
+      "Spark Desktop" = 6445813049;
+      Keynote = 409183694;
+      LINE = 539883307;
+      "The Unarchiver" = 425424353;
+      PopClip = 445189367;
+      Numbers = 409203825;
+    };
+  };
+
   nix-homebrew = {
     enable = true;
     enableRosetta = true;
-    user = "killtw";
+    user = username;
 
     taps = {};
 
     mutableTaps = true;
     autoMigrate = true;
-
-    extraEnv = {
-      HOMEBREW_NO_ANALYTICS = "1";
-      HOMEBREW_NO_ENV_HINTS = "1";
-    };
   };
 }
